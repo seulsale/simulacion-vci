9a18baee15d3f0661e01ed089c7aacfaa30610a1
Sergio Saucedo <sergio.saucedo10@gmail.com>
2019-11-01 07:56:19 -0600

    Prueba iniput

--- src/MainVCI.java
+++ src/MainVCI.java
@@ -1,8 +1,11 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.io.BufferedReader;
 
 public class MainVCI {
     public static void main(String[] args) {
-        HashMap<String, int> tokens = new HashMap<String, int>();
+        HashMap<String, Integer> tokens = new HashMap<>();
         tokens.put("/", 60);
         tokens.put("*", 60);
         tokens.put("+", 50);
@@ -17,6 +20,22 @@ public class MainVCI {
         tokens.put("or", 40);
         tokens.put("=", 40);
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = "";
+
+        System.out.println("Escribe un vci: \n");
+        try {
+            input = br.readLine();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        String[] expr = input.split(" ");
+        for (String item:
+             expr) {
+            System.out.println(item);
+        }
 
     }
 }
