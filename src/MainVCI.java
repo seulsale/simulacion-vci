6e35bd376ad50680637f1d8c0af07c96e79ec86f
Sergio Saucedo <sergio.saucedo10@gmail.com>
2019-11-07 11:30:57 -0600

    Implementa condiciones

--- src/MainVCI.java
+++ src/MainVCI.java
@@ -2,40 +2,88 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class MainVCI {
     public static void main(String[] args) {
-        HashMap<String, Integer> tokens = new HashMap<>();
-        tokens.put("/", 60);
-        tokens.put("*", 60);
-        tokens.put("+", 50);
-        tokens.put("-", 50);
-        tokens.put("<", 40);
-        tokens.put(">", 40);
-        tokens.put("==", 40);
-        tokens.put("<=", 40);
-        tokens.put(">=", 40);
-        tokens.put("not", 40);
-        tokens.put("and", 40);
-        tokens.put("or", 40);
-        tokens.put("=", 40);
+        HashMap<String, Integer> items = new HashMap<>();
+
+        items.put("/", 60);
+        items.put("*", 60);
+        items.put("+", 50);
+        items.put("-", 50);
+        items.put("<", 40);
+        items.put(">", 40);
+        items.put("==", 40);
+        items.put("<=", 40);
+        items.put(">=", 40);
+        items.put("not", 40);
+        items.put("and", 40);
+        items.put("or", 40);
+        items.put("=", 40);
+        items.put(";", 0);
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String input = "";
+        List<String> vci = new ArrayList<>();
+        Stack<String> op = new Stack<>();
 
         System.out.println("Escribe un vci: \n");
         try {
             input = br.readLine();
         } catch (IOException e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
 
         String[] expr = input.split(" ");
         for (String item:
              expr) {
-            System.out.println(item);
-        }
 
+            boolean integer = false;
+
+            // siguiente condición
+            try {
+                Integer.parseInt(item);
+                integer = true;
+            } catch (Exception e) {}
+
+
+            if (item.equals("(")) {
+                op.push("("); // sin preguntar
+            } else if (item.equals(")")) {
+                // vacia la pila hasta el primer parentesis
+                while (!op.peek().equals("(")) {
+                    vci.add(op.pop());
+                }
+                op.pop(); // quitar parentesis que cierra
+            } else if (integer || !items.containsKey(item)) {
+                // Estos items se agregan directamente a la pila
+                vci.add(item);
+            } else if (item.equals(";")) {
+                // Vaciar pila de operadores
+                while (!op.isEmpty()) {
+                    vci.add(op.pop());
+                }
+            } else {
+                // Para los operadores
+
+                if (op.isEmpty()) {
+                    op.push(item);
+                    continue;
+                }
+
+                // si es el primero
+                try {
+                    while (!op.isEmpty() && items.get(item) <= items.get(op.peek())) {
+                        vci.add(op.pop());
+                    }
+                } catch (NullPointerException e) { }
+                op.push(item);
+            }
+        }
+        System.out.println("VCI: \n" + vci);
     }
 }
